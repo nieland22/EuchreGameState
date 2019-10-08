9ade6c2a93840568e3f7ae891e378f291e143871
Haley <hgwelliver@gmail.com>
2019-10-08 15:30:18 -0700

    adding classes

---
+++ app/src/main/java/edu/euchreproject/gamestate/Card.java
@@ -0,0 +1,9 @@
+package edu.euchreproject.gamestate;
+
+public class Card {
+    public enum Suit {HEARTS, SPADES, CLUBS, DIAMONDS}
+
+    public Card(){
+
+    }
+}
--- app/src/main/java/edu/euchreproject/gamestate/GameState.java
+++ app/src/main/java/edu/euchreproject/gamestate/GameState.java
@@ -1,4 +1,31 @@
 package edu.euchreproject.gamestate;
 
 public class GameState {
+    int playerResources;
+    int playerResourceState;
+    int turn; //0-3 for players
+    int sharedResources;
+    //visibility of information from perspective of each player
+    int currentPlayerScore;
+    int timerState;
+    int gameStage; //0-3
+
+    //default constructor
+    public GameState(){
+
+
+    }
+
+    //copy constructor
+    public GameState(GameState other){
+
+
+    }
+
+    @Override
+    public String toString(){
+
+        return "";
+    }
+
 }
