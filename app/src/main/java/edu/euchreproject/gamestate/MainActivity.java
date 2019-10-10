1e264f0fc75a1d14dd5790c3cae5879cfd838a35
Sierra Nieland <nieland22@up.edu>
2019-10-10 15:37:20 -0700

    start of on click method in main activity

--- app/src/main/java/edu/euchreproject/gamestate/MainActivity.java
+++ app/src/main/java/edu/euchreproject/gamestate/MainActivity.java
@@ -3,6 +3,9 @@ package edu.euchreproject.gamestate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting onClick for run test button
+        Button run = (Button) findViewById(R.id.runTestButton);
+        run.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                EditText edit = (EditText)findViewById(R.id.editText);
+                //edit.setText(GameState.toString());
+            }
+        });
     }
 }
